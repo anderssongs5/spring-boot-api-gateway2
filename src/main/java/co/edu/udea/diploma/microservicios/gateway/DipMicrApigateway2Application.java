2dd40f2db3f0df9d982a1f7cdce42ef087525fcc
Andersson Garcia Sotelo <anderssongarciasotelo@gmail.com>
2019-06-21 23:22:22 -0500

    Configuring MongoDB and Kubernetes

--- src/main/java/co/edu/udea/diploma/microservicios/gateway/DipMicrApigateway2Application.java
+++ src/main/java/co/edu/udea/diploma/microservicios/gateway/DipMicrApigateway2Application.java
@@ -3,17 +3,14 @@ package co.edu.udea.diploma.microservicios.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-import co.edu.udea.diploma.microservicios.gateway.configuration.RibbonConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableDiscoveryClient
-@SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
-@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableSwagger2
 public class DipMicrApigateway2Application {
 
 	public static void main(String[] args) {
